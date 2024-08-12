f5a7419d81a95e6a71a123415fbeb1541ae4a2ad
NguyenDinhTienITHUFI <dinhtien147123@gmail.com>
2024-08-12 14:34:40 +0700

    backend-todo-springboot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+--insert into todo(id,description,done,target_date,username)
+--values(1001,'Learn C#',false,CURRENT_DATE(),'tienvua')
+
+insert into todo (id, description, target_date, username, done) 
+values (10002, 'Learn Use React', CURRENT_DATE(), 'tienvua', false);
+
+insert into todo (id, description, target_date, username, done) 
+values (10003, 'Learn C#', CURRENT_DATE(), 'tienvua', false);
+
+insert into todo (id, description, target_date, username, done) 
+values (10004, 'Learn AWS', CURRENT_DATE(), 'tienvua',false);
